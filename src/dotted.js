29197b959ad88122efda9dac783589bbc657ba9d
Greg Elliot <greg.elliot@datasent.com>
2025-01-04 21:30:48 -0500

    cleanup

--- src/dotted.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var pick_ts_1 = require("./pick.ts");
-var place_ts_1 = require("./place.ts");
-exports.default = {
-    pick: pick_ts_1.pick,
-    place: place_ts_1.place,
-};
